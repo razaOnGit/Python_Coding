622df718fdf7cf765d604dea76464b5ab53379df
MD RAZA KHAN <kmdraza47@gmail.com>
2024-12-20 15:10:22 +0530

    Update hello.py

---
+++ hello.py
@@ -0,0 +1,36 @@
+print ("hello world")
+print(5)
+print(10+10)
+print("hey lsiten I am a good boy \nthis viewer is also a good girl") 
+
+#  variables and data types
+a = complex(8, 2)
+b = True
+c = "Harry"
+d = None
+print(a)
+print(b)
+a1 = 9
+print(a + a1)
+print("The type of a is ", type(a))
+print("The type of b is ", type(b))
+print("The type of c is ", type(c))
+
+
+list1 = [8, 2.3, [-4, 5], ["apple", "banana"]]
+print(list1)
+
+tuple1 = (("parrot", "sparrow"), ("Lion", "Tiger"))
+print(tuple1)
+
+dict1 = {"name":"Sakshi", "age":20, "canVote":True}
+print(dict1)
+
+#calculator operation
+print (5+6)
+print (15-6)
+print (15*6)
+print (15/6)
+print (15//6)
+print (5%3)
+print (2**4)
