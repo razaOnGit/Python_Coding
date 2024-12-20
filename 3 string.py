48cf5eb0478d74af0c04cbf878db09c82fa882a8
MD RAZA KHAN <kmdraza47@gmail.com>
2024-12-20 23:21:44 +0530

    init

--- 3 string.py
+++ 3 string.py
@@ -1,9 +1,12 @@
-name = "Harry"
+name = "raza"
 friend = "Rohan"
 anotherFriend = 'Lovish'
 apple = '''He said, 
-Hi Harry
+Hi raza
 hey I am good
 "I want to eat an apple'''
 print("Hello, " + name)
-print(apple) 
+#print(apple) 
+print(name[0])
+print(name[1])
+print(name[2])
