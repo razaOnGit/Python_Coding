e0ff53fb3307512fa25e9d79efd64be45d3461e0
MD RAZA KHAN <kmdraza47@gmail.com>
2024-12-21 13:16:56 +0530

    Update 5 loops.py

--- 5 loops.py
+++ 5 loops.py
@@ -1,16 +1,26 @@
-# name ='abhishek'
-# for i in name:
-#     print(i) 
-#     if(i=='h'):
-#         print("found")
+name ='abhishek'
+for i in name:
+    print(i) 
+    if(i=='h'):
+        print("found")
 
-# colors=['red','blue','green','yellow']
-# for color in colors:
-#     print(color)
-#     for i in color:
-#         print(i)
+colors=['red','blue','green','yellow']
+for color in colors:
+    print(color)
+    for i in color:
+        print(i)
       
 for k in range(1,12,3):
     print(k)
 
-    
+# python while loop
+i=0
+while i<10:
+    print(i)
+    i+=1
+count =5
+while (count>0):
+    print(count)
+    count-=1
+else:
+    print("im inside while loop")
