69fae9d5031e545b70f7da0459a2f3e1f5363e35
MD RAZA KHAN <kmdraza47@gmail.com>
2024-12-21 09:33:18 +0530

    Update 3 string.py

--- 3 string.py
+++ 3 string.py
@@ -13,6 +13,18 @@ print(name[2])
 print(name[3])
 #same like i want to print for apple character 
 #using for loop
-print("lets use for loop\n")
-for character in friend:
- print(character)
+
+# print("lets use for loop\n")
+# for character in friend:
+#  print(character)
+
+
+print(apple[0:4])
+print(len(name))
+fruit = "Mango"
+#print(fruit[:5])
+#print(fruit[0:-3])
+#print(fruit[:len(fruit)-3])
+# print(fruit[-1:len(fruit) - 3])
+# print(fruit[-3:-1])
+print(fruit.upper())
