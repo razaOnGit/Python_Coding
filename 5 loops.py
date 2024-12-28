3d8b0cfddc2af50e5b5a1ecd178501b2c3f19789
MD RAZA KHAN <kmdraza47@gmail.com>
2024-12-28 12:53:30 +0530

    Update 5 loops.py

--- 5 loops.py
+++ 5 loops.py
@@ -1,26 +1,44 @@
-name ='abhishek'
-for i in name:
-    print(i) 
-    if(i=='h'):
-        print("found")
+# name ='abhishek'
+# for i in name:
+#     print(i) 
+#     if(i=='h'):
+#         print("found")
 
-colors=['red','blue','green','yellow']
-for color in colors:
-    print(color)
-    for i in color:
-        print(i)
+# colors=['red','blue','green','yellow']
+# for color in colors:
+#     print(color)
+#     for i in color:
+#         print(i)
       
-for k in range(1,12,3):
-    print(k)
+# for k in range(1,12,3):
+#     print(k)
 
 # python while loop
-i=0
-while i<10:
-    print(i)
-    i+=1
-count =5
-while (count>0):
-    print(count)
-    count-=1
+
+# i=0
+# while i<10:
+#     print(i)
+#     i+=1
+
+# count =5
+# while (count>0):
+#     print(count)
+#     count-=1
+# else:
+#     print("im inside while loop")
+
+marks = int(input("Enter the marks: "))
+
+if marks >= 90:
+    grade = "A"
+elif marks >= 75:
+    grade = "B"
+elif marks >= 50:
+    grade = "C"
+elif marks >= 35:
+    grade = "D"
 else:
-    print("im inside while loop")
+    grade = "Fail"
+
+
+print("The grade is:" ,grade)
