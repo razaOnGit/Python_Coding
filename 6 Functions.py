37c4caa725eac27814441768416f6a3e2d6620e3
MD RAZA KHAN <kmdraza47@gmail.com>
2024-12-29 12:11:35 +0530

    Create 6 Functions.py

---
+++ 6 Functions.py
@@ -0,0 +1,20 @@
+def calculateMean(a,b):
+    Mean =(a*b)/(a+b)
+    print(Mean)
+
+#functionn 2
+def calculatemean(a,b):
+    Mean =(a+b)/2
+    print(Mean)
+
+#function call
+def isgreater(a,b):
+    if a>b:
+        print("First no. is grater")
+    else:
+        print("Second no. is grater")
+a=4
+b=8 
+calculateMean(a,b)
+calculatemean(a,b)
+isgreater(a,b)
