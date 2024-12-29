b6da04d798448a23a3598106a87f9b6ff96a2c35
MD RAZA KHAN <kmdraza47@gmail.com>
2024-12-29 12:31:41 +0530

    Update 6 Functions.py

--- 6 Functions.py
+++ 6 Functions.py
@@ -1,20 +1,35 @@
-def calculateMean(a,b):
-    Mean =(a*b)/(a+b)
-    print(Mean)
+# def calculateMean(a,b):
+#     Mean =(a*b)/(a+b)
+#     print(Mean)
 
-#functionn 2
-def calculatemean(a,b):
-    Mean =(a+b)/2
-    print(Mean)
+# #functionn 2
+# def calculatemean(a,b):
+#     Mean =(a+b)/2
+#     print(Mean)
 
-#function call
-def isgreater(a,b):
-    if a>b:
-        print("First no. is grater")
-    else:
-        print("Second no. is grater")
-a=4
-b=8 
-calculateMean(a,b)
-calculatemean(a,b)
-isgreater(a,b)
+# #function call
+# def isgreater(a,b):
+#     if a>b:
+#         print("First no. is grater")
+#     else:
+#         print("Second no. is grater")
+# a=4
+# b=8 
+# calculateMean(a,b)
+# calculatemean(a,b)
+# isgreater(a,b)
+
+#defaul argument function
+# def sum(a,b=5):
+#     print(a+b)
+# sum(4)
+
+# def name(fname ="john" , lname ="doe"):
+#     print(fname,lname)
+# name("jane")
+def average(*numbers):
+    sum = 0
+    for i in numbers:
+        sum =sum +i
+    print(sum/len(numbers))
+average(5,6) 
