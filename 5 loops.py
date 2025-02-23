53891c60cf320b77586b0a3d1fb58f17483d2d71
MD RAZA KHAN <kmdraza47@gmail.com>
2025-02-23 22:09:05 +0530

    loops

--- 5 loops.py
+++ 5 loops.py
@@ -15,10 +15,10 @@
 
 # python while loop
 
-# i=0
-# while i<10:
-#     print(i)
-#     i+=1
+i=0
+while i<10:
+    print(i)
+    i+=1
 
 # count =5
 # while (count>0):
@@ -27,25 +27,13 @@
 # else:
 #     print("im inside while loop")
 
-# marks = int(input("Enter the marks: "))
-# if marks >= 90:
-#     grade = "A"
-# elif marks >= 75:
-#     grade = "B"
-# elif marks >= 50:
-#     grade = "C"
-# elif marks >= 35:
-#     grade = "D"
-# else:
-#     grade = "Fail"
 
-# print("The grade is:" ,grade)
 
-for i in range(1,19): 
+# for i in range(1,19): 
 
    
  
-    print(15*i)
+#     print(15*i)
 
 # for i in range(1,11): 
 
@@ -60,3 +48,25 @@ for i in range(1,19):
 #     i=i+1
 #     if (i%100==0):
 #         break
+
+
+
+
+
+
+# for loop with else statement 
+# i = 0
+# while i<7:
+#   print(i)
+#   i = i + 1
+#     # if i == 4:
+#      #   break
+
+# else:
+#   print("Sorry no i")
+
+# for x in range(5):
+#     print ("iteration no {} in for loop".format(x+1))
+# else:
+#     print ("else block in loop")
+# print ("Out of loop")
